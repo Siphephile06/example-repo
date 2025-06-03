3c7d3cbd1fbe1cce5232fec13fae5e129b09cefb
Siphephile06 <siphephilesithole54@gmail.com>
2025-06-03 13:43:07 +0200

    Delete myscript.ps1

--- myscript.ps1
+++
@@ -1,9 +0,0 @@
-# Check if the file exists 
-if (Test-Path -Path "example.txt") { 
-    # If the file exists, print a message 
-    Write-Output "The file 'example.txt' already exists." 
-} else { 
-    # If the file does not exist, create it and print a message 
-    New-Item -ItemType File -Name "example.txt" 
-    Write-Output "The file 'example.txt' has been created." 
-}
